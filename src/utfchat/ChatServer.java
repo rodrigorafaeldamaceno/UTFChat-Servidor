2beaacf19c279eaabf7b3d59ad758cb1c54cda65
Rodrigo Rafael <rodrigo_medy@hotmail.com>
2019-07-15 07:03:53 -0300

    alterações

--- src/utfchat/ChatServer.java
+++ src/utfchat/ChatServer.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package utfchat;
 
 import java.rmi.RemoteException;
@@ -35,8 +31,7 @@ public class ChatServer extends UnicastRemoteObject implements ChatServerInt {
                 ChatClientInt tmp = (ChatClientInt) v.get(i);
                 tmp.tell(s);
             } catch (Exception e) {
-                //problem with the client not connected.
-                //Better to remove it
+                System.out.println("Erro ao publicar.");
             }
         }
     }
--- src/utfchat/StartServer.java
+++ src/utfchat/StartServer.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package utfchat;
 
 import java.awt.Color;
@@ -29,7 +24,7 @@ public class StartServer {
             java.rmi.registry.LocateRegistry.createRegistry(1099);
 
             ChatServerInt b = new ChatServer();
-            Naming.rebind("rmi://127.0.0.1/myabc", b);
+            Naming.rebind("rmi://127.0.0.1/utfchat", b);
             System.out.println("Servidor online...");
         } catch (Exception e) {
             System.out.println("Falha no servidor! " + e);
